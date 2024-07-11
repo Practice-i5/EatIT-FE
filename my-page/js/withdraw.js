813a7485a220d342d4ef1fe166de605bccfbe1bc
MungChi <2ee42a45@gmail.com>
2024-07-11 16:56:25 +0900

    미팅 리스트 재업로드

--- my-page/js/withdraw.js
+++
@@ -1,9 +0,0 @@
-let withdrawQuest = document.querySelector("#withdraw-quest");
-let withdrawDone = document.querySelector("#withdraw-done");
-let withdrawSubmit = document.querySelector("#withdraw-submit");
-
-withdrawSubmit.addEventListener("click" ,()=>{
-    withdrawDone.classList.remove("unactive");
-    withdrawQuest.classList.add("unactive");
-})
-
